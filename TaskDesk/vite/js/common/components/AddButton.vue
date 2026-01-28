a13e06be6c2781dec4b02f82e249a5a0b41cf36c
Anton Konyshev <anton.konyshev@gmail.com>
2026-01-28 13:35:58 +0300

    Colors of the "add item" button component were modified.

--- TaskDesk/vite/js/common/components/AddButton.vue
+++ TaskDesk/vite/js/common/components/AddButton.vue
@@ -1,6 +1,6 @@
 <template>
-    <button type="button" @click="addItem()" ref="add-btn" class="fixed bottom-4 right-4 flex items-center size-18 rounded-full bg-green-300 hover:bg-green-700 group duration-200 cursor-pointer">
-        <span class="inline-block size-7 bg-no-repeat bg-center bg-contain svg-plus w-full group-hover:invert-100 duration-200"></span>
+    <button type="button" @click="addItem()" ref="add-btn" class="fixed bottom-4 right-4 flex items-center size-18 rounded-full bg-td-primary dark:bg-td-secondary hover:brightness-[1.2] group duration-200 cursor-pointer">
+        <span class="inline-block size-7 bg-no-repeat bg-center bg-contain svg-plus w-full invert-100 dark:invert-0 duration-200"></span>
     </button>
 </template>
 
